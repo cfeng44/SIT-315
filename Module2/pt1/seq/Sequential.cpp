8278d63b71830f15d0d12fdb942a3d9280071f47
cfeng44 <cfeng44@outlook.com>
2024-08-31 00:34:22 +1000

    Added print to stream capability to seq matrix multiply.

--- Module2/pt1/seq/Sequential.cpp
+++ Module2/pt1/seq/Sequential.cpp
@@ -8,7 +8,7 @@
 //              Multiplying square matrices (NxN) that are filled with random
 //              integers. Using modern C++ (>C++11) features now after getting
 //              pointers and memory experience with C-style in previous code
-//              files.
+//              files. Output matrices are written to a file.
 //
 //              All operations are implemented with sequential programming.
 //
@@ -19,32 +19,74 @@
 #include <iostream>
 #include <time.h> 
 #include <chrono>
+#include <fstream>
+#include <string>
 
 using namespace std::chrono;
 using namespace std;
 
 
-const size_t N = 100; // N is number of rows and columns.
+const int N = 100; // N is number of rows and columns.
+const size_t MAX_ENTRY_VALUE = 100;
 
 using Matrix = int**;
 
+// Sets each value in a matrix to a random integer between
+// [0, MAX_ENTRY_VALUE].
 void randomMatrix(Matrix M) {
     for (size_t i = 0; i < N; i++) {
         for (size_t j = 0; j < N; j++) {
-            // Initialises vector entry to random number in [0, 100].
-            M[i][j] = rand() % 100;
+            // *Note: In previous submissions I made an error with the modulo
+            // operator as x % n is between [0, n) ie non-inclusive. This is
+            // why I have added a +1.
+            M[i][j] = rand() % MAX_ENTRY_VALUE + 1;
         }
     } 
 }
 
+// Computes the dot product of two square matrices, resulting in matrix C.
 void dotProduct(Matrix A, Matrix B, Matrix C) {
     for (size_t i = 0; i < N; i++) {
         for (size_t j = 0; j < N; j++) {
+            C[i][j] = 0; // Set to 0 initially so we can increment it below.
             for (size_t k = 0; k < N; k++) {
-                C[i][j] += A[i][j] * B[k][j];
+                C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
+}
+
+// Prints a matrix to an output stream, formatted in the way commonly used in
+// mathematics: 
+// [a b c]
+// [d e f]
+// [g h i]
+void printMatrix(Matrix m, ostream& stream) {
+    for (size_t i = 0; i < N; i++) {
+        stream << "[";
+        for (size_t j = 0; j < N; j++) {
+            if (j != N -1) {
+                // Setting width to the number of digits in MAX_ENTRY_VALUE 
+                // ensures that all the values in the output are evenly spaced.
+                int digit = to_string(MAX_ENTRY_VALUE).length();
+                stream << setw(digit) <<  m[i][j] << " ";
+            }
+            else {
+                // Gets rid of extra space for last element in each row.
+                int digit = to_string(MAX_ENTRY_VALUE).length();
+                stream << setw(digit) <<  m[i][j];
+            }
+        }
+        stream << "]" << endl;
+    }
+}
+
+// Prints a matrix to a file.
+void matrixFile(Matrix m, string file_name) {
+    ofstream results_file;
+    results_file.open(file_name);
+    printMatrix(m, results_file);
+    results_file.close();
 }
 
 int main() {
@@ -78,6 +120,10 @@ int main() {
         << "Sequential Runtime (\xC2\xB5s):          " // UTF-8 for "micro".
         << duration.count() << " microseconds" 
         << endl << endl;
+
+    matrixFile(A, "matrixA.mtrx");
+    matrixFile(B, "matrixB.mtrx");
+    matrixFile(C, "matrixC.mtrx");
 
     return EXIT_SUCCESS;
 }
